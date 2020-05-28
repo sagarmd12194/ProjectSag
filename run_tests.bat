1694df5c29ba7054f1d9c002937794fc122e299b
SagarMD12194 <sagarmd888@gmail.com>
2020-05-28 21:36:57 +0530

    tests cases are added

--- run_tests.bat
+++ run_tests.bat
@@ -1,1 +1,5 @@
 pytest tests/unit/testCalculator.py
+set current_dir=%cd%
+echo "current directory >>>>> %current_dir%"
+
+coverage run -m \tests\unit\testCalculator.py
--- tests/unit/testCalculator.py
+++ tests/unit/testCalculator.py
@@ -7,8 +7,8 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.calc=Calculator()
 
-    def test_addition(self):
-        result=self.calc.add_func(5,6)
+    def test_add(self):
+        result=self.calc.add(5,6)
         self.assertEqual(11,result,"addition function is not working....")
 
 
